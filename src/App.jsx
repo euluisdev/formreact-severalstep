67ed46e5257884bdbb755631624829574010283d
euluis-dev <fluisf00@gmail.com>
2024-04-22 11:49:07 -0300

    feat: initialized form to evaluate the product

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import './App.css';
 
 function App() {
-    return (
+  return (
     <div className='App'>
-      <h1>Hellow Wordão!</h1>
+      <div className='header'>
+        <h2>Deixe sua avaliação</h2>
+        <p>
+          Ficamos felizes com sua compra, utilize o formulário abaixo para avaliar o prduto
+        </p>
+      </div>
+      <div className="form-container">
+        <p>etapas</p>
+        <form>
+          <div className="actions">
+            <button type='button'>Voltar</button>
+            <button type='submit'>Avançar</button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
