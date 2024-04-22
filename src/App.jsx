7009e39f2a54b515b70edec6f553f2588adfc159
euluis-dev <fluisf00@gmail.com>
2024-04-22 12:02:48 -0300

    feat: icons-Gr used in the buttons

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,21 @@ function App() {
       <div className='header'>
         <h2>Deixe sua avaliação</h2>
         <p>
-          Ficamos felizes com sua compra, utilize o formulário abaixo para avaliar o prduto
+          Ficamos felizes com sua compra, utilize o formulário abaixo para avaliar o produto
         </p>
       </div>
       <div className="form-container">
         <p>etapas</p>
         <form>
           <div className="actions">
-            <button type='button'>Voltar</button>
-            <button type='submit'>Avançar</button>
+            <button type='button'>
+              <GrFormPrevious />
+              <span>Voltar</span>
+            </button>
+            <button type='submit'>
+              <span>Avançar</span>
+              <GrFormNext />
+            </button>
           </div>
         </form>
       </div>
