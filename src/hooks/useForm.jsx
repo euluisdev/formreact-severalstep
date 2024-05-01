2207a2fd9d933740031bd76049d85971502c8ad9
euluis-dev <fluisf00@gmail.com>
2024-05-01 13:47:30 -0300

    changeStep function was created in useForm hook file

--- src/hooks/useForm.jsx
+++ src/hooks/useForm.jsx
@@ -3,9 +3,18 @@ import React, { useState } from 'react';
 function useForm(steps) {
     const [currentStep, setCurrentStep] = useState(0);
 
+    function changeStep (index, e) {
+        e.preventDefault();
+
+        if (i < 0 | i >= steps.length) return;
+
+        setCurrentStep(index)
+    }
+
     return {
         currentStep,
         currentComponent: steps[currentStep],
+        changeStep, 
     }
 };
 
