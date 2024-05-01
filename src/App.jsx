82171aef0d06b46aa4735486fb75c45d7fb3c5ba
euluis-dev <fluisf00@gmail.com>
2024-05-01 13:31:35 -0300

    the name ot the state has been adjusted

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import './App.css';
 
 function App() {
   const formComponents = [<UserForm />, <ReviewForm />, <Thanks />];
-  const { currentStap, currentComponent } = useForm(formComponents);
+  const { currentStep, currentComponent } = useForm(formComponents);
 
   return (
     <div className='App'>
