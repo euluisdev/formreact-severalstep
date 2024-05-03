d243d0c1b21340b5c795090e73abeb3e7b154be6
euluis-dev <fluisf00@gmail.com>
2024-05-02 22:02:04 -0300

    isFirstStep function was created in UseForm file

--- src/hooks/useForm.jsx
+++ src/hooks/useForm.jsx
@@ -4,7 +4,7 @@ function useForm(steps) {
     const [currentStep, setCurrentStep] = useState(0);
 
     function changeStep (i, e) {
-        if (e) e.preventDefault();
+        if (e) e.preventDefault(); //previne o comp default do evento 
 
         if (i < 0 || i >= steps.length) return;
 
@@ -15,7 +15,8 @@ function useForm(steps) {
         currentStep,
         currentComponent: steps[currentStep],
         changeStep, 
-        isLastStep: currentStep + 1 === steps.length ? true : false,
+        isLastStep: currentStep + 1 === steps.length ? true : false, 
+        isFirstStep: currentStep === 0 ? true : false, 
     }
 };
 
