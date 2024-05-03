0f9160143ef36508ac46f138c8d554fa7b5f46c5
euluis-dev <fluisf00@gmail.com>
2024-05-03 11:41:13 -0300

    inputs were created to process the data

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -2,7 +2,28 @@ import React from "react";
 
 const UserForm = () => {
     return (
-        <div>User Form</div>
+        <div>
+            <div className="form-control">
+                <label htmlFor="name">Nome:</label>
+                <input 
+                  type="text"
+                  name="text" 
+                  id="name" 
+                  placeholder="Digite seu nome" 
+                  required 
+                  />
+            </div>
+            <div className="form-control">
+                <label htmlFor="email">E-mail:</label>
+                <input 
+                  type="email"
+                  name="email" 
+                  id="email" 
+                  placeholder="Digite seu melhor e-mail" 
+                  required 
+                  />
+            </div>
+        </div>
     )
 }
 
