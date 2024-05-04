8900c91f35535ca76e7d2407499ef4ad1a737e99
euluis-dev <fluisf00@gmail.com>
2024-05-04 10:44:07 -0300

    the icons were imported from react-icons lib

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -1,15 +1,21 @@
+import { AiOutlineUser, AiOutlineStar } from 'react-icons/ai';
+import { FiSend } from 'react-icons/fi';
+
 import './Steps.css';
 
 function Steps({ currentStep }) {
     return (
         <div className='steps'>
             <div className="step">
+                <AiOutlineUser />
                 <p>Identificação</p>
             </div>
             <div className="step">
+                <AiOutlineStar />
                 <p>Avaliação</p>
             </div>
             <div className="step">
+                <FiSend />
                 <p>Envio</p>
             </div>
         </div>
