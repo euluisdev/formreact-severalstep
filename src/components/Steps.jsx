ab908618e19bf718a62277963197ef5a62b8242d
euluis-dev <fluisf00@gmail.com>
2024-05-04 10:59:09 -0300

    the active class and the logic for each step were created in the Steps.jsx file

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -6,15 +6,15 @@ import './Steps.css';
 function Steps({ currentStep }) {
     return (
         <div className='steps'>
-            <div className="step">
+            <div className="step active">
                 <AiOutlineUser />
                 <p>Identificação</p>
             </div>
-            <div className="step">
+            <div className={`step ${currentStep >= 1 ? 'active' : ''}`}>
                 <AiOutlineStar />
                 <p>Avaliação</p>
             </div>
-            <div className="step">
+            <div className={`step ${currentStep >= 2 ? 'active' : ''}`}>
                 <FiSend />
                 <p>Envio</p>
             </div>
