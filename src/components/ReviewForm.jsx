80e5d7346211665c2e6d28b753557db9a7cfe66b
euluis-dev <fluisf00@gmail.com>
2024-05-05 10:25:13 -0300

    all buttons and their respective emoji were created to evaluate satisfation of the users

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -10,9 +10,25 @@ const ReviewForm = () => {
     return (
         <div className="review-form">
             <div className="form-control score-container">
-                <label className="radio-container">
+            <label className="radio-container">
                     <input type="radio" value='unsatisfied' name="review" required />
+                    <BsFillEmojiFrownFill />
                     <p>Insatisfeito</p>
+                </label>
+                <label className="radio-container">
+                    <input type="radio" value='meutral' name="review" required />
+                    <BsFillEmojiNeutralFill />
+                    <p>Poderia ser melhor</p>
+                </label>
+                <label className="radio-container">
+                    <input type="radio" value='satisfied' name="review" required />
+                    <BsFillEmojiSmileFill />
+                    <p>Satisfeito</p>
+                </label>
+                <label className="radio-container">
+                    <input type="radio" value='very-satisfied' name="review" required />
+                    <BsFillEmojiHeartEyesFill />
+                    <p>Muito ssatisfeito</p>
                 </label>
             </div>
         </div>
