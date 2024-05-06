380613ccd6bb320132d92d8228040bf49deb9356
euluis-dev <fluisf00@gmail.com>
2024-05-06 14:24:26 -0300

    the rendering of the Thanks.jsx file is almost complete, i will do a brief styling in the Thanks.css file

--- src/components/Thanks.jsx
+++ src/components/Thanks.jsx
@@ -9,7 +9,22 @@ import './Thanks.css';
 
 const Thanks = () => {
     return (
-        <div>Thanks Component</div>
+        <div className="thanks-container">
+            <h2>Falta pouco...</h2>
+            <p>
+                A sua avaliação é muito importante, e com ela, você receberá um cupom de desconto de 15%, para sua próxima compra.
+            </p>
+            <p>
+                Para concluir sua avaliação, basta clicar no botão Enviar, que se encontra abaixo.
+            </p>
+            <h3>Um breve resumo da sua avaliação:</h3>
+            <p className="review-data">
+                <span>Satisfação com o produto:</span>
+            </p>
+            <p className="review-data">
+                <span>Comentário:</span>
+            </p>
+        </div>
     )
 }
 
