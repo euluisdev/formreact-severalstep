602c0ce8e23b02e5f84ea305926c9f2743b283c9
euluis-dev <fluisf00@gmail.com>
2024-05-07 10:35:31 -0300

    the updateFieldHandler function was passed as props to the ReviewForm.jsx file and after that, it was used in de file's inputs to keep the data persistent wuen changing steps

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,34 +1,62 @@
-import { 
-    BsFillEmojiHeartEyesFill, 
-    BsFillEmojiSmileFill, 
-    BsFillEmojiNeutralFill, 
+import {
+    BsFillEmojiHeartEyesFill,
+    BsFillEmojiSmileFill,
+    BsFillEmojiNeutralFill,
     BsFillEmojiFrownFill
 } from "react-icons/bs";
 
 import './ReviewForm.css';
 
 
-const ReviewForm = ({ data }) => {
+const ReviewForm = ({ data, updateFieldHandler }) => {
     return (
         <div className="review-form">
             <div className="form-control score-container">
-            <label className="radio-container">
-                    <input type="radio" value='unsatisfied' name="review" required />
+                <label className="radio-container">
+                    <input 
+                        type="radio"
+                        value='unsatisfied'
+                        name="review"
+                        required 
+                        checked={data.review === 'unsatisfied'} 
+                        onChange={(e) => updateFieldHandler('review', e.target.value)} 
+                    />
                     <BsFillEmojiFrownFill />
                     <p>Insatisfeito</p>
                 </label>
                 <label className="radio-container">
-                    <input type="radio" value='meutral' name="review" required />
+                    <input 
+                        type="radio" 
+                        value='neutral' 
+                        name="review" 
+                        required 
+                        checked={data.review === 'neutral'} 
+                        onChange={(e) => updateFieldHandler('review', e.target.value)} 
+                        />
                     <BsFillEmojiNeutralFill />
                     <p>Poderia ser melhor</p>
                 </label>
                 <label className="radio-container">
-                    <input type="radio" value='satisfied' name="review" required />
+                    <input 
+                        type="radio" 
+                        value='satisfied' 
+                        name="review" 
+                        required 
+                        checked={data.review === 'satisfield'} 
+                        onChange={(e) => updateFieldHandler('satisfield', e.target.value)} 
+                        />
                     <BsFillEmojiSmileFill />
                     <p>Satisfeito</p>
                 </label>
                 <label className="radio-container">
-                    <input type="radio" value='very-satisfied' name="review" required />
+                    <input 
+                        type="radio" 
+                        value='very_satisfied' 
+                        name="review" 
+                        required 
+                        checked={data.review === 'very_satisfied'} 
+                        onChange={(e) => updateFieldHandler('review', e.target.value)} 
+                        />
                     <BsFillEmojiHeartEyesFill />
                     <p>Muito ssatisfeito</p>
                 </label>
