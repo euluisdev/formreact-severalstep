48584a3246e5a1f3279b77dbb220bc80e32836f2
euluis-dev <fluisf00@gmail.com>
2024-05-07 09:49:49 -0300

    the email input also received the function for data retention, so when you go back a step, the data remains

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -5,25 +5,27 @@ const UserForm = ({ data, updateFieldHandler }) => {
         <div>
             <div className="form-control">
                 <label htmlFor="name">Nome:</label>
-                <input 
-                  type="text"
-                  name="name" 
-                  id="name" 
-                  placeholder="Digite seu nome" 
-                  required 
-                  value={data.name || ''} 
-                  onChange={(e) => updateFieldHandler('name', e.target.value)}
-                  />
+                <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="Digite seu nome"
+                    required
+                    value={data.name || ''}
+                    onChange={(e) => updateFieldHandler('name', e.target.value)}
+                />
             </div>
             <div className="form-control">
                 <label htmlFor="email">E-mail:</label>
-                <input 
-                  type="email"
-                  name="email" 
-                  id="email" 
-                  placeholder="Digite seu melhor e-mail" 
-                  required 
-                  />
+                <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Digite seu melhor e-mail"
+                    required
+                    value={data.email || ''}  
+                    onChange={(e) => updateFieldHandler('email', e.target.value)}
+                />
             </div>
         </div>
     )
