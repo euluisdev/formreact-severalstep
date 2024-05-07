f86ebd4c61ed8d93381f00cf556fdfb036b3b3d6
euluis-dev <fluisf00@gmail.com>
2024-05-07 12:36:03 -0300

    finalized style of the Thanks.css file

--- src/components/Thanks.jsx
+++ src/components/Thanks.jsx
@@ -5,6 +5,8 @@ import {
     BsFillEmojiFrownFill
 } from "react-icons/bs";
 
+import { BiHourglass } from "react-icons/bi";
+
 import './Thanks.css';
 
 const emojiDataHandler = {
@@ -17,21 +19,21 @@ const emojiDataHandler = {
 const Thanks = ({ data }) => {
     return (
         <div className="thanks-container">
-            <h2>Falta pouco...</h2>
+            <h2>Falta pouco <BiHourglass /></h2>
             <p>
                 A sua avaliação é muito importante, e com ela, você receberá um cupom de desconto de 15%, para sua próxima compra.
             </p>
             <p>
                 Para concluir sua avaliação, basta clicar no botão Enviar, que se encontra abaixo.
             </p>
-            <h3>Um breve resumo da sua avaliação {data.name}:</h3>
+            <h3>Um resumo da sua avaliação, <span className="show-name">{data.name}</span>:</h3>
             <p className="review-data">
                 <span>Satisfação com o produto:</span>
                 <span className="show-emoji">{emojiDataHandler[data.review]}</span>
             </p>
             <p className="review-data">
                 <span>Comentário:</span>
-                {data.comment}
+                <span className="show-comment"> {data.comment}</span>
             </p>
         </div>
     )
